8c2f000955711f0f593cc936b61d53a9a3711b64
Lars Pensjö <lars.pensjo@gmail.com>
2016-10-09 14:09:55 +0200

    Compute mappings of  unique symbols

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::collections::HashMap;
 
+type Reference = usize;
+
 #[derive(Debug)]
 struct TokenInfo {
 	count: u32,
-	pos: u32,
+	pos: Reference, // -1 represents no position
 }
 type Symbol = char;
 type SymbolTable = HashMap<Symbol, TokenInfo>;
@@ -12,20 +14,33 @@ fn main() {
 	let start_sym = 'Ä';
 	let end_sym = 'Ö';
 	
-	let v1 = vec![start_sym, 'A', 'B', 'C', 'D', 'E', 'G', end_sym];
-	let v2 = vec![start_sym, 'D', 'E', 'F', 'G', 'A', 'C', end_sym];
+	let old_file = vec![start_sym, 'A', 'B', 'C', 'D', 'E', 'G', end_sym];
+	let new_file = vec![start_sym, 'D', 'E', 'F', 'G', 'A', 'C', end_sym];
 	let mut symbol_table_old : SymbolTable = HashMap::new();
 	let mut symbol_table_new : SymbolTable = HashMap::new();
-	count(&mut symbol_table_old, v1);
-	count(&mut symbol_table_new, v2);
-    println!("old symbol table {:?}", symbol_table_old);
-    println!("New symbol table {:?}", symbol_table_new);
+	count(&mut symbol_table_old, old_file);
+	count(&mut symbol_table_new, new_file);
+    // println!("old symbol table {:?}", symbol_table_old);
+    // println!("New symbol table {:?}", symbol_table_new);
 	let unique_symbols = unique(&symbol_table_old, &symbol_table_new);
 	println!("Unique symbols: {:?}", unique_symbols);
+	
+	let mut old_mapping : Vec<Reference> = vec![0; symbol_table_old.len()];
+	let mut new_mapping : Vec<Reference> = vec![0; symbol_table_new.len()];
+	
+	for symbol in unique_symbols {
+		let TokenInfo{count:_, pos:line_new} = symbol_table_new[&symbol];
+		let TokenInfo{count:_, pos:line_old} = symbol_table_old[&symbol];
+		old_mapping[line_old] = line_new;
+		new_mapping[line_new] = line_old;
+	}
+	
+	println!("Mapping from old: {:?}", old_mapping);
+	println!("Mapping from new: {:?}", new_mapping);
 }
 
 fn count(s : &mut SymbolTable,  v : Vec<Symbol>) {
-	let mut line : u32 = 0;
+	let mut line : Reference = 0;
 	for symbol in v {
 		let new : TokenInfo = match s.get(&symbol) {
 			Some(info) => TokenInfo{count: info.count+1, pos: line},
