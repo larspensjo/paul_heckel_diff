1d2b922c31660cf85f7713484a7b6acf4c7cd631
Lars Pensjö <lars.pensjo@gmail.com>
2016-10-09 12:13:58 +0200

    Two symbol tables

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,26 @@ type Symbol = char;
 type SymbolTable = HashMap<Symbol, u32>;
 
 fn main() {
-	let x : i32 = 0;
 	let start_sym = 'Ä';
 	let end_sym = 'Ö';
-	let v1 = vec![start_sym, 'a', 'b', 'a', 'c', 'd', 'e', end_sym];
-	let v2 = vec![start_sym, 'a', 'b', 'c', 'd', 'e', end_sym];
-	let mut symbol_table : SymbolTable = HashMap::new();
-    println!("Hello, world! {:?}", symbol_table);
-	count(&mut symbol_table, v1);
-    println!("New symbol table {:?}", symbol_table);
+	
+	let v1 = vec![start_sym, 'A', 'B', 'C', 'D', 'E', 'G', end_sym];
+	let v2 = vec![start_sym, 'D', 'E', 'F', 'G', 'A', 'C', end_sym];
+	let mut symbol_table_old : SymbolTable = HashMap::new();
+	let mut symbol_table_new : SymbolTable = HashMap::new();
+	count(&mut symbol_table_old, v1);
+	count(&mut symbol_table_new, v2);
+    println!("old symbol table {:?}", symbol_table_old);
+    println!("New symbol table {:?}", symbol_table_new);
 }
 
 fn count(s : &mut SymbolTable,  v : Vec<char>) {
 	for symbol in v {
-		// let mut old : u32 = 0;
 		let old = match s.get(&symbol) {
 			Some(number) => *number,
 			_ => 0,
 		};
 		s.insert(symbol, old+1);
-		// s[&symbol] = s[&symbol] + 1;
 		println!("Symbol: {}", symbol);
 	}
 }
